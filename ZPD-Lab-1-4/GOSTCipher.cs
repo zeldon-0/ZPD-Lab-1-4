2fd6544073f590101fe4a9bb3e37d7a1850e0679
zeldon-0 <48602270+zeldon-0@users.noreply.github.com>
2020-10-09 01:00:43 +0300

    Initial Commit

---
+++ ZPD-Lab-1-4/GOSTCipher.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZPD_Lab_1_4
+{
+    public class GOSTCipher
+    {
+        private BitArray _key;
+        private List<int> _subKeyOrder = new List<int>() 
+        {
+            1, 2, 3, 4, 5, 6, 7, 8,
+            1, 2, 3, 4, 5, 6, 7, 8,
+            1, 2, 3, 4, 5, 6, 7, 8,
+            8, 7, 6, 5, 4, 3, 2, 1
+        };
+
+        public GOSTCipher(BitArray key)
+        {
+            _key = key;
+        }
+
+        public BitArray Encode(BitArray message)
+        {
+            throw new NotImplementedException();
+        }
+        public BitArray Decode(BitArray encodedMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        private BitArray _encodeBlock(BitArray block)
+        {
+            BitArrayBlock wholeBlock = new BitArrayBlock(block);
+
+            BitArrayBlock leftBlock = wholeBlock.GetLeftHalf();
+            BitArrayBlock rightBlock = wholeBlock.GetRightHalf();
+
+            for (int i = 0; i < 32; i++)
+            {
+                BitArrayBlock rightBlockCopy = rightBlock.Clone();
+
+                BitArrayBlock subkey = _getSubKey(i);
+                rightBlock = rightBlock.Xor(subkey);
+
+                // TO DO: S-BLOCK
+
+                rightBlock.CircularLeftShift(11);
+                rightBlock = rightBlock.Xor(leftBlock);
+
+                leftBlock = rightBlockCopy;
+            }
+
+            rightBlock.ExchangeBits(leftBlock);
+
+            return leftBlock.CombineIntoBitArray(rightBlock);
+        }
+
+        private BitArrayBlock _getSubKey(int round)
+        {
+            int subKeyIndex = _subKeyOrder[round];
+
+            bool[] bits = new bool[32];
+            for (int i = 0; i < 32; i++)
+            {
+                bits[i] = _key.Get(32 * subKeyIndex + i);
+            }
+
+            BitArray bitArray = new BitArray(bits);
+            return new BitArrayBlock(bitArray);
+        }
+
+    }
+}
---
+++ ZPD-Lav-1-4-Tests/BitArrayBlockTest.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Linq;
+using Xunit;
+using ZPD_Lab_1_4;
+
+namespace ZPD_Lav_1_4_Tests
+{
+    public class BitArrayBlockTest
+    {
+        [Fact]
+        public void GetLeftHalf_FourZeroesAndFourOnes_ReturnsBitArrayConstistingOf32Zeros()
+        {
+            int[] values = new int[] { 0, 0, 0, 0, 1, 1, 1, 1 };
+            BitArray bits = new BitArray(values.Select(a => (byte)a).Reverse().ToArray());
+            BitArrayBlock block = new BitArrayBlock(bits);
+            bool[] arrayToCheck = new bool[32];
+            bool[] expectedArray = new bool[32];
+
+            block = block.GetRightHalf();
+            bits = block.GetBits();
+            bits.CopyTo(arrayToCheck, 0);
+
+            Assert.Equal(expectedArray, arrayToCheck);
+
+        }
+
+
+        [Fact]
+        public void GetRightHalf_FourZeroesAndFourOnes_ReturnsBitArrayConstistingOf4OneOctets()
+        {
+            int[] values = new int[] { 0, 0, 0, 0, 1, 1, 1, 1 };
+            BitArray bits = new BitArray(values.Select(a => (byte)a).Reverse().ToArray());
+            BitArrayBlock block = new BitArrayBlock(bits);
+            bool[] arrayToCheck = new bool[32];
+            bool[] expectedArray = new bool[32];
+
+            block = block.GetRightHalf();
+            bits = block.GetBits();
+            bits.CopyTo(arrayToCheck, 0);
+            bits = new BitArray(new int[] { 1, 1, 1, 1 }.Select(a => (byte)a).ToArray());
+            bits.CopyTo(expectedArray, 0);
+
+            Assert.Equal(expectedArray, arrayToCheck);
+
+        }
+        [Fact]
+        public void Exchange_TwoBitArrays_BitArrayBlocksShouldHaveTheirVitArraysSwitched()
+        {
+            int[] firstArray = { 0, 0, 0, 0 };
+            int[] secondArray = { 1, 1, 1, 1 };
+
+            BitArray firstBitArray = new BitArray(firstArray.Select(a => (byte)a).Reverse().ToArray());
+            BitArray secondBitArray = new BitArray(secondArray.Select(a => (byte)a).Reverse().ToArray());
+
+            BitArrayBlock firstBlock = new BitArrayBlock(firstBitArray);
+            BitArrayBlock secondBlock = new BitArrayBlock(secondBitArray);
+
+            firstBlock.ExchangeBits(secondBlock);
+
+            Assert.Equal(secondBitArray, firstBlock.GetBits());
+            Assert.Equal(firstBitArray, secondBlock.GetBits());
+        }
+    }
+}
